e75567e886df9507773f58efed6bb0d91d0b11da
Asfan Ulla <asfanulla@gmail.com>
2024-01-31 02:38:31 +0530

    doc and properties table for select component

---
+++ src/routes/select/Tables.svelte
@@ -0,0 +1,68 @@
+<!-- Properties Table -->
+<h3>Select Properties</h3>
+<table class="w-full">
+	<thead class="text-left">
+		<tr>
+			<th>Prop</th>
+			<th>Type</th>
+			<th>Default</th>
+			<th>Description</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td><code>options*</code></td>
+			<td>Array</td>
+			<td>&nbsp;</td>
+			<td>Options for select</td>
+		</tr>
+        <tr>
+			<td><code>name</code></td>
+			<td>String</td>
+			<td>&nbsp;</td>
+			<td>name of the input field</td>
+		</tr>
+		<tr>
+			<td><code>label</code></td>
+			<td>String</td>
+			<td><code>Select</code></td>
+			<td>Text label appears above the input field</td>
+		</tr>
+        <tr>
+			<td><code>placeholder</code></td>
+			<td>String</td>
+			<td><code>Please Select a value</code></td>
+			<td>Option value description</td>
+		</tr>
+        <tr>
+			<td><code>multiple</code></td>
+			<td>Boolean</td>
+			<td><code>false</code></td>
+			<td>Allows user to select multiple values</td>
+		</tr>
+		<tr>
+			<td><code>required</code></td>
+			<td>Boolean</td>
+			<td><code>false</code></td>
+			<td>Set when a field value is required</td>
+		</tr>
+		<tr>
+			<td><code>disabled</code></td>
+			<td>Boolean</td>
+			<td><code>false</code></td>
+			<td>Disables the user from selecting</td>
+		</tr>
+		<tr>
+			<td><code>note</code></td>
+			<td>String</td>
+			<td>&nbsp;</td>
+			<td>Text appears below select field in a lighter color</td>
+		</tr>
+		<tr>
+			<td><code>error</code></td>
+			<td>String</td>
+			<td>&nbsp;</td>
+			<td>Error message for the select field</td>
+		</tr>
+	</tbody>
+</table>
